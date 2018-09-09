6bff480a83ab601ff4249bb1f96f35996e78743a
chenjun <chenjun@meiqia.com>
2018-09-09 15:43:02 +0800

    fix date util

--- app/src/main/java/com/umbrella/financialteaching/utils/DateUtil.java
+++ app/src/main/java/com/umbrella/financialteaching/utils/DateUtil.java
@@ -1,6 +1,10 @@
 package com.umbrella.financialteaching.utils;
 
+import android.text.format.DateFormat;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -8,10 +12,146 @@ import java.util.Date;
  */
 
 public class DateUtil {
+    public static final long ONE_MINUTE_MILLIONS = 60 * 1000;
+    public static final long ONE_HOUR_MILLIONS = 60 * ONE_MINUTE_MILLIONS;
+    public static final long ONE_DAY_MILLIONS = 24 * ONE_HOUR_MILLIONS;
+
     public static String getStringDate() {
         Date currentDate = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dateString = simpleDateFormat.format(currentDate);
         return dateString;
+    }
+
+    public static Date getDate(String dateStr) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ss");
+        try {
+            Date date = simpleDateFormat.parse(dateStr);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String toDateStr(Date date) {
+        String str = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return simpleDateFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getTimeDown(String dateStr) {
+        String str = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'THH:mm:ss");
+
+        try {
+            Date date = simpleDateFormat.parse(dateStr);
+            Date currentDate = new Date();
+
+            long durTime = currentDate.getTime() - date.getTime();
+            int dayStatus = calculateDayStatus(date, currentDate);
+
+            if (durTime <= 10 * ONE_MINUTE_MILLIONS) {
+                str = "刚刚";
+            } else if (durTime < ONE_HOUR_MILLIONS) {
+                str = durTime / ONE_MINUTE_MILLIONS + "分钟前";
+            } else if (dayStatus == 0) {
+                str = durTime / ONE_HOUR_MILLIONS + "小时前";
+            } else if (dayStatus == -1) {
+                str = "昨天" + DateFormat.format("HH:mm", date);
+            } else if (isSameYear(date, currentDate)) {
+                str = DateFormat.format("MM-dd", date).toString();
+            } else {
+                str = DateFormat.format("yyyy-MM", date).toString();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+
+    public static String getCurrentTimeDown(long millis) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd'T'HH:mm:ss");
+        Date date = new Date(millis);
+        String str = simpleDateFormat.format(date);
+        return getTimeDown(str);
+    }
+
+    public static int calculateDayStatus(Date targetTime, Date compareTime) {
+        Calendar tarCalendar = Calendar.getInstance();
+        tarCalendar.setTime(targetTime);
+        int tarDayOfYear = tarCalendar.get(Calendar.DAY_OF_YEAR);
+
+        Calendar compareCalendar = Calendar.getInstance();
+        tarCalendar.setTime(compareTime);
+        int compareDayOfYear = compareCalendar.get(Calendar.DAY_OF_YEAR);
+
+        return tarDayOfYear - compareDayOfYear;
+    }
+
+    public static boolean isSameYear(Date targetTime, Date compareTime) {
+        Calendar tarCalendar = Calendar.getInstance();
+        tarCalendar.setTime(targetTime);
+        int tarDayOfYear = tarCalendar.get(Calendar.YEAR);
+
+        Calendar compareCalendar = Calendar.getInstance();
+        tarCalendar.setTime(compareTime);
+        int compareDayOfYear = compareCalendar.get(Calendar.YEAR);
+
+        return tarDayOfYear == compareDayOfYear;
+    }
+
+    public static String getConstellation(int m, int d) {
+
+        final String[] constellationArr = {"魔羯座", "水瓶座", "双鱼座", "牡羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "魔羯座"};
+
+        final int[] constellationEdgeDay = {20, 18, 20, 20, 20, 21, 22, 22, 22, 22, 21, 21};
+        int month = m;
+        int day = d;
+        if (day <= constellationEdgeDay[month - 1]) {
+            month = month - 1;
+        }
+        if (month >= 0) {
+            return constellationArr[month];
+        }
+        //default to return 魔羯
+        return constellationArr[11];
+
+    }
+
+    public static int getAge(Date birthDay) throws Exception {
+        Calendar calendar = Calendar.getInstance();
+
+        if (calendar.before(birthDay)) {
+            throw new IllegalArgumentException("The birthday is before Now. It\'s unbelievable");
+        }
+
+        int yearNow = calendar.get(Calendar.YEAR);
+        int monthNow = calendar.get(Calendar.MONTH);
+        int dayOfMonthNow = calendar.get(Calendar.DAY_OF_MONTH);
+
+        calendar.setTime(birthDay);
+        int yearBirth = calendar.get(Calendar.YEAR);
+        int monthBirth = calendar.get(Calendar.MONTH);
+        int dayOfMonthBirth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int age = yearNow - yearBirth;
+        if (monthNow <= monthBirth) {
+            if (monthNow == monthBirth) {
+                if (dayOfMonthNow < dayOfMonthBirth) {
+                    age--;
+                }
+            } else {
+                age--;
+            }
+        }
+        return age;
     }
 }
